197b97c546b21e4d596269ec7e0240752a62a348
BTimms <brian.timakela@hotmail.com>
2024-03-27 17:46:57 +0000

    Made a Landing Page using a controller and basic routing

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+</head>
+<body>
+<h1>Welcome to SportPost</h1>
+<p>Dedicated to Uncovering the</p>
+<p>Heart and Soul of Sports</p>
+<p>Stories,Insights and Analysis Beyond the Game</p>
+</body>
+</html>
