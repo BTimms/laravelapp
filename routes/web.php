31f9b31c5a5a7e276631995117429b7e09a3a012
BTimms <brian.timakela@hotmail.com>
2024-03-28 21:07:13 +0000

    Created Database Migrations for posts and made a resource route linking to the PostsController

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,4 +19,6 @@ use Illuminate\Support\Facades\Route;
 //});
 
 Route::get('/', [\App\Http\Controllers\PagesController::class, 'index']);
+
+Route::resource('posts', PostsController::class);
 
