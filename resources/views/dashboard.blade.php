cb08404fcb4f86d7d7575b341433a70a8dd94440
BTimms <brian.timakela@hotmail.com>
2024-04-11 17:04:07 +0100

    redirected the register page to the post page when registering

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
+    <a href="/posts" class="btn btn-primary"> <-- Back to Posts </a>
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
